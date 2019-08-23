83ac209f885b903164814afdf0e22a0b94e2d789
Juan Thiago <thiago.reiissouza@gmail.com>
2019-08-23 20:45:34 -0300

    exercicio4

---
+++ dist/exercicio4-5.js
@@ -0,0 +1,10 @@
+var nm = 4;
+var sobra = function (x1) {
+    if (x1 % 2 == 0) {
+        console.log("Par");
+    }
+    else {
+        console.log("Impar");
+    }
+};
+sobra(nm);
---
+++ exercicio4-5.ts
@@ -0,0 +1,12 @@
+let nm: number =4
+let sobra = function(x1: number) : void{
+    if (x1 %2 == 0){
+        console.log ("Par")
+    }
+    else{
+
+        console.log ("Impar")
+    }
+}
+
+sobra(nm)
