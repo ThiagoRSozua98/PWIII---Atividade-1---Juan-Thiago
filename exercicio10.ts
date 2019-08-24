c93ad81a01a17ea21c7794262cda0c2191ef55d4
Juan Thiago <thiago.reiissouza@gmail.com>
2019-08-23 21:59:18 -0300

    exercicio10

---
+++ exercicio10.ts
@@ -0,0 +1,16 @@
+export let sobra = function(x1: number) : boolean{
+    if (x1 %2 == 0){
+       return true
+    }
+    else{
+        return false
+    }
+
+    export let calcArea = function(lagA: number , comB: number): number{
+        return lagA * comB
+    }
+
+    export let calcubo = (numero: number) => {
+        return Math.pow (numero, 3)
+    }
+    
