b4556d9a9955884d35cf49690f48b9bb90a1ea2a
Juan Thiago <thiago.reiissouza@gmail.com>
2019-08-23 21:29:09 -0300

    exercicio7

---
+++ dist/exercicio7.js
@@ -0,0 +1,5 @@
+var cubo = 4;
+var calcubo = function (numero) {
+    return Math.pow(numero, 3);
+};
+console.log(calcubo(cubo));
---
+++ exercicio7.ts
@@ -0,0 +1,7 @@
+let cubo: number = 4
+
+let calcubo = (numero: number) => {
+    return Math.pow (numero, 3)
+}
+
+console.log(calcubo(cubo)) 
